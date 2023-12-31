8454929c23d71c51a23fc925885d5734e968d71f
WagnerRR25 <rodrigueswagner09@gmail.com>
2023-12-31 02:56:50 -0300

    feat:  quizz styling and typescript logic

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
 
+  title:string = ""
+
+  questions:any
+  questionsSelected:any
+
+  answers:string[] = []
+  answersSelected:string =""
+
+  questionIndex:number =0
+  questionMaxIndex:number =0
+
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+    }
+  }
 }
